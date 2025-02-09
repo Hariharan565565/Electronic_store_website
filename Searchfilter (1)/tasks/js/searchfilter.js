1060d6d12011428104a0bfd3c36883970560ee44
Hariharan565565 <fantasticfacts565@gmail.com>
2025-02-09 17:54:02 +0530

    update1

--- Searchfilter (1)/tasks/js/searchfilter.js
+++ Searchfilter (1)/tasks/js/searchfilter.js
@@ -46,7 +46,7 @@ let galleryarray = [
         id:7,
         name:"Mobile Charger",
         src: "./images/mobilecharger.jpeg",
-        desc:" Full charge in 25 minutes "
+        desc:" Fully charge in 25 minutes "
         
     },
     {
